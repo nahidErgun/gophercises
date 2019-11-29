f50e774ac76487c5568bd67408553b0b092173ea
nahideergun <ergunnahide94@gmail.com>
2019-11-29 13:25:06 +0300

    Design a choose your own adventure book renderer that shows the story via a web application

---
+++ chooseYourAdventure/main/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
+type Story struct {
+	Title   string   `json:"title"`
+	Story   []string `json:"story"`
+	Options []struct {
+		Text string `json:"text"`
+		Arc  string `json:"arc"`
+	} `json:"options"`
+}
+func main() {
+
+	//tmpl := template.Must(template.ParseFiles("./index.html"))
+	file, err := ioutil.ReadFile("./story.json")
+	fmt.Println(err)
+	jsonMap := make(map[string]Story)
+
+	err = json.Unmarshal(file,&jsonMap)
+	if err != nil {
+
+		fmt.Println("failed to read json")
+	}
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+		pathh := strings.Trim(string(path),"/")
+		fmt.Println(pathh)
+		tmpl.Execute(w, jsonMap[pathh])
+	})
+
+	http.ListenAndServe(":9090", nil)
+	fmt.Println(http.ListenAndServe(":9090", nil))
+
+	/*http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("dddddd")
+		tmpl := template.Must(template.ParseFiles("./index.html"))
+
+		fmt.Println(tmpl)
+
+		tmpl.Execute(w, jsonMap)
+		http.ListenAndServe(":9080", nil)
+	})*/
+
+}
+
+
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, )
+}
+
+func response (w http.ResponseWriter,r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("./index.html"))
+
+	tmpl.Execute(w, "")
+}
+
+
+
